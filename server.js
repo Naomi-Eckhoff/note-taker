ea651b231a8750b1d77e18a9e5bf015e486fdb79
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-12-04 20:38:00 -0600

    htmlRoutes set up

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}`);
 });
