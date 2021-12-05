cde264fe9b7ed18104acf40661f5f8041654887a
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-12-04 23:34:07 -0600

    added an auto id to notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
 
-router.post('/db', (req, res))
+router.post('/db', (req, res) => {
+
+})
 
 module.exports = router;
