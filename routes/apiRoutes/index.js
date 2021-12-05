173028c75caa3ce776b649ffe76717229577b41f
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-12-04 23:55:28 -0600

    added post and get

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
+const { createNoteId, createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 
-router.post('/db', (req, res) => {
+router.get('/db', (req, res) => {
+  let results = notes;
+  res.json(results);
+});
 
-})
+router.post('/db', (req, res) => {
+  if (!validateNote(req.body)) {
+    res.status(400).send('This note is not properly formatted');
+  } else {
+    const newNote = createNoteId(req.body, notes);
+    res.json(newNote);
+  }
+});
 
 module.exports = router;
